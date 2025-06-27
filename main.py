6d7fb00b5e04a8a68a19a04bed0220be49da1511
tealslug <tealslug@lionsoft.com>
2025-06-26 21:41:01 -0700

    Added stats library to contain all the "counting" functions

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(filepath):
   with open(filepath) as f:
     file_contents = f.read()
   return file_contents
 
-def count_words(text):
-  return len(text.split())
-
 def main():
   file_contents = get_book_text("books/frankenstein.txt")
-  num_words = count_words(file_contents)
+  num_words = get_num_words(file_contents)
   print(f"{num_words} words found in the document")
 
 main()
