985a9b7badb57452fd88fe7bba799e6c1e35083a
tealslug <tealslug@lionsoft.com>
2025-06-26 22:59:38 -0700

    Remove and ignore pycache, add get_character_counts (should probably have been 2 separate changes)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_counts
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -9,5 +9,7 @@ def main():
   file_contents = get_book_text("books/frankenstein.txt")
   num_words = get_num_words(file_contents)
   print(f"{num_words} words found in the document")
+  chars = get_character_counts(file_contents)
+  print(chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def get_num_words(text):
   return len(text.split())
+
+def get_character_counts(text):
+  chars = {}
+  for c in [x.lower() for x in text]:
+    if c not in chars:
+      chars[c] = 1
+    else:
+      chars[c] += 1
+  return chars
